43874854374449b728d8be35c4216e229b28ae35
Vortrox <39481603+Vortrox@users.noreply.github.com>
2024-03-07 22:58:55 +1100

    add: Demo code for Selenium
    If an Edge browser opens the Google website, Selenium works.

--- src/main/java/edu/deakin/sit333/Main.java
+++ src/main/java/edu/deakin/sit333/Main.java
@@ -1,7 +1,25 @@
 package edu.deakin.sit333;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.setProperty(
+                "webdriver.edge.driver",
+                "C:\\Users\\Vortrox\\Documents\\Scripts\\msedgedriver_122.0.2365.66.exe");
+
+        WebDriver driver = new EdgeDriver();
+        System.out.println(driver);
+
+        driver.get("https://www.google.com");
+
+        try {
+            Thread.sleep(5*1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        driver.close();
     }
 }
