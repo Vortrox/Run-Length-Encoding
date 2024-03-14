369f64d25f7d048046a7606a9edd877c6f909fa7
Vortrox <39481603+Vortrox@users.noreply.github.com>
2024-03-14 13:39:03 +1100

    add: Import task 2.1 code
    Update SeleniumOperations.java

--- src/main/java/edu/deakin/sit333/Main.java
+++ src/main/java/edu/deakin/sit333/Main.java
@@ -1,25 +1,13 @@
 package edu.deakin.sit333;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-
-public class Main {
-    public static void main(String[] args) {
-        System.setProperty(
-                "webdriver.edge.driver",
-                "C:\\Users\\Vortrox\\Documents\\Scripts\\msedgedriver_122.0.2365.66.exe");
-
-        WebDriver driver = new EdgeDriver();
-        System.out.println(driver);
-
-        driver.get("https://www.google.com");
-
-        try {
-            Thread.sleep(5*1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        driver.close();
+/**
+ * Hello world!
+ * @author Ahsan Habib
+ */
+public class Main 
+{
+    public static void main( String[] args )
+    {
+        SeleniumOperations.officeworks_registration_page("https://www.officeworks.com.au/app/identity/create-account");
     }
 }
