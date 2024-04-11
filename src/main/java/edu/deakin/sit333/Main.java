5008321d4ac3beeffede6e68d499c792a577a7de
Vortrox <39481603+Vortrox@users.noreply.github.com>
2024-04-12 02:10:11 +1000

    add: Import task 4.1 code

--- src/main/java/edu/deakin/sit333/Main.java
+++ src/main/java/edu/deakin/sit333/Main.java
@@ -1,25 +1,51 @@
 package edu.deakin.sit333;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-
-public class Main {
-    public static void main(String[] args) {
-        System.setProperty(
-                "webdriver.edge.driver",
-                "C:\\Users\\Vortrox\\Documents\\Scripts\\msedgedriver_122.0.2365.66.exe");
-
-        WebDriver driver = new EdgeDriver();
-        System.out.println(driver);
-
-        driver.get("https://www.google.com");
-
-        try {
-            Thread.sleep(5*1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+/**
+ * Hello world!
+ * 
+ * @author Ahsan Habib
+ */
+public class Main 
+{
+    public static void main( String[] args )
+    {
+    	/*
+    	 * Failed login and don't care validation code.
+    	 */
+    	LoginStatus status = LoginForm.login(null, null);
+        System.out.println("[Empty username, empty password] >> " + status);
+        
+        status = LoginForm.login(null, "xyz");
+        System.out.println("[Empty username, wrong password] >> " + status);
+        
+        status = LoginForm.login("abc", "xyz");
+        System.out.println("[Wrong username, wrong password] >> " + status);
+        
+        status = LoginForm.login("ahsan", null);
+        System.out.println("[Correct username, empty password] >> " + status);
+        
+        status = LoginForm.login("ahsan", "xyz");
+        System.out.println("[Correct username, wrong password] >> " + status);
+        
+        status = LoginForm.login(null, "ahsan_pass");
+        System.out.println("[Empty username, Correct password] >> " + status);
+        
+        status = LoginForm.login("abc", "ahsan_pass");
+        System.out.println("[Wrong username, Correct password] >> " + status);
+        
+        /*
+         * Login success case, there is a validation code in errorMsg field.
+         */
+        status = LoginForm.login("ahsan", "ahsan_pass");
+        System.out.println("[Correct username, Correct password] >> " + status);
+        
+        if (status.isLoginSuccess()) {
+        	/*
+        	 * Test validation code cases
+        	 */
+        	System.out.println("\tEmpty code >> " + LoginForm.validateCode(null));
+        	System.out.println("\tWrong code >> " + LoginForm.validateCode("abcd"));
+        	System.out.println("\tCorrect code >> " + LoginForm.validateCode("123456"));
         }
-
-        driver.close();
     }
 }
