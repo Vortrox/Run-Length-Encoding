67a0fa688cf0e15cd585b4ba1a0c78c9070e32ec
Vortrox <39481603+Vortrox@users.noreply.github.com>
2024-04-19 13:37:29 +1000

    add: Import task 6.1p code

---
+++ src/test/java/edu/deakin/sit333/WeatherAndMathUtilsTest.java
@@ -0,0 +1,30 @@
+package edu.deakin.sit333;
+
+import edu.deakin.sit333.WeatherAndMathUtils;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class WeatherAndMathUtilsTest {
+
+	@Test
+	public void testStudentIdentity() {
+		String studentId = null;
+		Assert.assertNotNull("Student ID is null", studentId);
+	}
+
+	@Test
+	public void testStudentName() {
+		String studentName = null;
+		Assert.assertNotNull("Student name is null", studentName);
+	}
+
+	@Test
+	public void testFalseNumberIsEven() {
+		Assert.fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCancelWeatherAdvice() {
+		Assert.assertEquals("CANCEL", WeatherAndMathUtils.weatherAdvice(70.1, 0.0));
+	}
+}
