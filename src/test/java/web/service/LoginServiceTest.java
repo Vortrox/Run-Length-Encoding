3211ff531630a944511ca981a35844892ed3770d
Vortrox <39481603+Vortrox@users.noreply.github.com>
2024-05-10 01:53:07 +1000

    chg: Re-use driver to speed up tests

--- src/test/java/web/service/LoginServiceTest.java
+++ src/test/java/web/service/LoginServiceTest.java
@@ -1,7 +1,6 @@
 package web.service;
 
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -9,45 +8,46 @@ import org.openqa.selenium.edge.EdgeDriver;
 
 
 public class LoginServiceTest {
-    @Test
-    public void testLoginSuccess() {
+    private static WebDriver driver;
+    @BeforeClass
+    public static void createDriver() {
         System.setProperty("webdriver.edge.driver", "C:\\Users\\Vortrox\\Documents\\Scripts\\msedgedriver_124.0.2477.0.exe");
 
-        WebDriver driver = new EdgeDriver();
+        driver = new EdgeDriver();
         System.out.println("Driver info: " + driver);
+    }
 
-        // Full path where login.html is located.
-        // You can click on html file and copy the path shown in your browser.
+    @AfterClass
+    public static void closeDriver() {
+        driver.close();
+    }
+
+    @Before
+    public void resetLoginForm() {
         driver.navigate().to(System.getProperty("user.dir") + "/pages/login.html");
+    }
 
-        // Find username element
+    @Test
+    public void testLoginSuccess() {
         WebElement ele = driver.findElement(By.id("username"));
         ele.clear();
         ele.sendKeys("testuser");
 
-        // Find password element
         ele = driver.findElement(By.id("passwd"));
         ele.clear();
         ele.sendKeys("testpass");
 
-        // Find date of birth element
         ele = driver.findElement(By.id("dob"));
         ele.clear();
         ele.sendKeys("15062000");
 
-        // Find Submit button, and click on button.
         ele = driver.findElement(By.cssSelector("[type=submit]"));
         ele.submit();
 
-        /*
-         * On successful login, the title of page changes to 'success',
-         * otherwise, 'fail'.
-         */
         String title = driver.getTitle();
         System.out.println("Title: " + title);
 
         Assert.assertEquals(title, "success");
 
-        driver.close();
     }
 }
