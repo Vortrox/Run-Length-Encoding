353d6f2aded1cd01f760ae89105d467eb411258a
Vortrox <39481603+Vortrox@users.noreply.github.com>
2024-05-15 21:33:12 +1000

    add: Basic definition for a compression algorithm

---
+++ src/main/java/vortrox/rle/StringRLEAlgorithm.java
@@ -0,0 +1,11 @@
+package vortrox.rle;
+
+public class StringRLEAlgorithm {
+    public static String compress(String input) {
+        throw new UnsupportedOperationException();
+    }
+
+    public static String decompress(String input) {
+        throw new UnsupportedOperationException();
+    }
+}
