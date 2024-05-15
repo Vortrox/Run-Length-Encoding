433f77a882944282844bf75c601b57b7fe4fd281
Vortrox <39481603+Vortrox@users.noreply.github.com>
2024-05-16 03:48:52 +1000

    add: RLE string compression

--- src/main/java/edu/deakin/sit333/StringRLEAlgorithm.java
+++ src/main/java/edu/deakin/sit333/StringRLEAlgorithm.java
@@ -2,7 +2,26 @@ package edu.deakin.sit333;
 
 public class StringRLEAlgorithm {
     public static String compress(String input) {
-        throw new UnsupportedOperationException();
+        StringBuilder output = new StringBuilder(input.length());
+        char lastChar = input.charAt(0);
+        output.append(lastChar);
+        int runLength = 1;
+
+        for (int i = 1; i < input.length(); i++) {
+            if (input.charAt(i) != lastChar) {
+                if (runLength > 1) {
+                    output.append(runLength);
+                }
+
+                output.append(input.charAt(i));
+                runLength = 0;
+            }
+
+            runLength++;
+            lastChar = input.charAt(i);
+        }
+
+        return output.toString();
     }
 
     public static String decompress(String input) {
