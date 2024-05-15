c8d3e6326d3ef8c9143b003e8aca37ba89b02ecc
Vortrox <39481603+Vortrox@users.noreply.github.com>
2024-05-15 21:33:12 +1000

    add: Basic definition for a compression algorithm
    rename

---
+++ src/main/java/edu/deakin/sit333/StringRLEAlgorithm.java
@@ -0,0 +1,11 @@
+package edu.deakin.sit333;
+
+public class StringRLEAlgorithm {
+    public static String compress(String input) {
+        throw new UnsupportedOperationException();
+    }
+
+    public static String decompress(String input) {
+        throw new UnsupportedOperationException();
+    }
+}
