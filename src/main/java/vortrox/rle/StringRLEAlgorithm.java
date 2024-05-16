54a609f2fe2b0719c724ca88da6b15015f78d82f
Vortrox <39481603+Vortrox@users.noreply.github.com>
2024-05-16 15:46:20 +1000

    add: RLE string decompression

--- src/main/java/vortrox/rle/StringRLEAlgorithm.java
+++ src/main/java/vortrox/rle/StringRLEAlgorithm.java
@@ -39,6 +39,35 @@ public class StringRLEAlgorithm {
     }
 
     public static String decompress(String input) {
-        throw new UnsupportedOperationException();
+        StringBuilder output = new StringBuilder();
+        // Assumes that the first character in the input will be a letter
+        char lastLetter = input.charAt(0);
+        output.append(lastLetter);
+        StringBuilder digits = new StringBuilder();
+
+        for (int i = 1; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if (Character.isDigit(c)) {
+                digits.append(c);
+            } else {
+                if (digits.length() > 0) {
+                    // Append the last letter n - 1 times to the end of the output
+                    for (int j = 0; j < Integer.parseInt(digits.toString()); j++) {
+                        output.append(lastLetter);
+                    }
+                }
+
+                digits = new StringBuilder();
+                lastLetter = c;
+                output.append(lastLetter);
+            }
+        }
+
+        // Append the last letter n - 1 times to the end of the output
+        for (int j = 1; j < Integer.parseInt(digits.toString()); j++) {
+            output.append(lastLetter);
+        }
+
+        return output.toString();
     }
 }
